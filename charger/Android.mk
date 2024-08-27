LineageOS/android_vendor_lineage

lineage: Support charger on vendor
Change-Id: I7cfd4cdaf4e3db48e3a970cb103b35986ec290cf

charger/Android.mk
@@ -39,12 +39,30 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := lineage_charger_battery_scale_vendor
+LOCAL_MODULE_STEM := battery_scale.png
+LOCAL_SRC_FILES := $(lineage_charger_density)/battery_scale.png
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := lineage_charger_battery_fail
 LOCAL_MODULE_STEM := battery_fail.png
 LOCAL_SRC_FILES := $(lineage_charger_density)/battery_fail.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lineage_charger_battery_fail_vendor
+LOCAL_MODULE_STEM := battery_fail.png
+LOCAL_SRC_FILES := $(lineage_charger_density)/battery_fail.png
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -55,4 +73,14 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/values/charger
 LOCAL_REQUIRED_MODULES := lineage_charger_battery_scale lineage_charger_battery_fail
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lineage_charger_animation_vendor
+LOCAL_MODULE_STEM := animation.txt
+LOCAL_SRC_FILES := animation.txt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/values/charger
+LOCAL_REQUIRED_MODULES := lineage_charger_battery_scale_vendor lineage_charger_battery_fail_vendor
 include $(BUILD_PREBUILT)

aosp-zahedan/vendor_lmodroid

Signed-off-by: Farzin <itisFarzin@gmail.com>

charger/Android.mk
@@ -39,12 +39,30 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := lmodroid_charger_battery_scale_vendor
+LOCAL_MODULE_STEM := battery_scale.png
+LOCAL_SRC_FILES := $(lmodroid_charger_density)/battery_scale.png
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := lmodroid_charger_battery_fail
 LOCAL_MODULE_STEM := battery_fail.png
 LOCAL_SRC_FILES := $(lmodroid_charger_density)/battery_fail.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lmodroid_charger_battery_fail_vendor
+LOCAL_MODULE_STEM := battery_fail.png
+LOCAL_SRC_FILES := $(lmodroid_charger_density)/battery_fail.png
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -55,4 +73,14 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/values/charger
 LOCAL_REQUIRED_MODULES := lmodroid_charger_battery_scale lmodroid_charger_battery_fail
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lmodroid_charger_animation_vendor
+LOCAL_MODULE_STEM := animation.txt
+LOCAL_SRC_FILES := animation.txt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/values/charger
+LOCAL_REQUIRED_MODULES := lmodroid_charger_battery_scale_vendor lmodroid_charger_battery_fail_vendor
 include $(BUILD_PREBUILT)